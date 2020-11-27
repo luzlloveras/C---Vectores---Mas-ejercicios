5780c2e4822b4817c15f6e3f5ce7e806ac9394c4
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 16:12:24 -0300

    Update 02.c

--- 02.c
+++ 02.c
@@ -11,19 +11,13 @@ int main()
 
     printf("Ingrese 20 valores enteros:\n");
     for (i=0; i < 20; i++)
-    {
         scanf("%d", &vec[i]);
-    }
     printf("Ingrese 1 valor adicional:\n");
     scanf("%d", &DIV);
 
     for (i = 0; i < 20; i++)
-    {
         if (vec[i] % DIV == 0)
-        {
             contador++;
-        }
-    }
     printf("%d componentes son divisibles por %d \n", contador, DIV);
 
     return 0;
