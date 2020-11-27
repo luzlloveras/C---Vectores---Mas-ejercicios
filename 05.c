74a7243df226c5075718b9f25b269074b8d255f5
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 16:14:21 -0300

    Update 05.c

--- 05.c
+++ 05.c
@@ -15,31 +15,23 @@ int main()
     
     // Inicializa en 0 cada posicion del vector
     for (i = 0; i < D; i++)
-    {
         contador[i] = 0;
-    }
     
     // Simulacion de tiros del dado
     for (i = 0; i < N; i++)
-    {
         contador[DADO() - 1 ]++;
-    }
 
     // Imprime cantidad de veces que salio cada cara
     for (i = 0; i < D; i++)
-    {
         printf("\n cara %4d salio %d veces es %.2f %%", i+1, contador[i], (float)(contador[i]*100)/N);
-    }
     printf("\n");
 
     for (i = 0; i < D; i++)
-    {
         if (contador[i] > max)
         {
             max = contador[i];
             posmax = i;
         }
-    }
     printf("\n La cara que mas salio fue la %d con %d ocurrencias\n", posmax+1, max);
 
     return 0;
